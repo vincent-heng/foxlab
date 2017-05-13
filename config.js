dd0937e62a8850b0623f7f08054ad32012ef4a6f
vincent-heng <vincentheng@users.noreply.github.com>
2017-05-13 18:12:28 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.witai = {};
+config.googletts = {};
+
+config.witai.url = 'https://api.wit.ai/message';
+config.witai.authorization = "PUT YOUR KEY HERE";
+
+config.googletts.lang = "en";
+config.googletts.pitch = 1;
+
+module.exports = config;
