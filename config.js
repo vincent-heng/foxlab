1b7bd0ad6fd32d04fe28fd86c2f09061a3f6c235
vincent-heng <hengvincent.1994@gmail.com>
2017-05-14 00:47:27 +0200

    api rest port config

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 var config = {};
 
+config.foxlab = {};
 config.witai = {};
 config.googletts = {};
+
+config.foxlab.port = 3000;
 
 config.witai.url = 'https://api.wit.ai/message';
 config.witai.authorization = "PUT YOUR KEY HERE";
